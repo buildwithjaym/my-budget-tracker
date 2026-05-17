89eefcb383b42966ec4395564642f5e802e0b81d
buildwithjaym <jaymmaruji@gmail.com>
2026-05-17 10:48:30 +0800

    from total budget allocation to expenses

--- components/budgets/budget-manager.tsx
+++ components/budgets/budget-manager.tsx
@@ -581,7 +581,7 @@ export default function BudgetManager({
           emphasis
         />
         <SummaryCard
-          title="Total Budget Allocation"
+          title="Expenses"
           value={formatMoney(totalBudgetAllocation)}
           helper={`${((totalBudgetAllocation / totalIncome) * 100 || 0).toFixed(1)}% of income allocated`}
           icon={<Target className="h-5 w-5" />}
